643aaaa7333093dfc208c5247acaf4ca3f4a7aea
Shayne117 <55745961+Shayne117@users.noreply.github.com>
2025-10-16 16:52:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+    <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Bootstrap Icons -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+      <!-- Custom CSS -->
+<link rel="stylesheet" href="assets/css/style.css">
+<body> 
+    <?php include './Navbar/navbar-section.php'; ?>
+    <?php include './Header/header-section.php'; ?>
+    <?php include './WhatWeOffer/whatweoffer-section.php'; ?>
+    <?php include './HoursAndFees/hoursandfees-section.php'; ?>
+    <?php include './Footer/footer-section.php'; ?>
+</body>
+</html>
